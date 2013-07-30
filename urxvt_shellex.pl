4407ac76087f7b455c87c6251b8e19d5197483ef
Axel Wagner <mail@merovius.de>
2013-07-30 21:49:20 +0200

    Put the launcher-window on the output the mousepointer is on
    This fixes #1

--- urxvt_shellex.pl
+++ urxvt_shellex.pl
@@ -1,13 +1,40 @@
 use Data::Dumper;
+use X11::Protocol;
+
+sub get_outputs {
+    my @outputs = ();
+    foreach my $line (qx(xrandr -q)) {
+        next unless $line =~ /\sconnected/;
+        my ($w, $h, $x, $y) = ($line =~ /(\d+)x(\d+)\+(\d+)\+(\d+)/);
+        push @outputs, { w => $w, h => $h, x => $x, y => $y };
+    }
+    return @outputs;
+}
 
 sub on_init {
     my ($self) = @_;
+    my @outputs = get_outputs();
+
+    my $X = X11::Protocol->new($self->display_id);
+    my $ptr = { $X->QueryPointer($self->DefaultRootWindow) };
+
+    $self->{x} = 0;
+    $self->{y} = 0;
+    $self->{w} = 1024;
+    foreach my $output (@outputs) {
+        if ($output->{x} <= $ptr->{root_x} && $ptr->{root_x} < $output->{x} + $output->{w}) {
+            $self->{x} = $output->{x};
+            $self->{y} = $output->{y};
+            $self->{w} = $output->{w};
+        }
+    }
+
     ();
 }
 
 sub on_start {
     my ($self) = @_;
-    $self->XMoveResizeWindow($self->parent, 0, 0, 1440, 2+$self->fheight);
+    $self->XMoveResizeWindow($self->parent, $self->{x}, $self->{y}, $self->{w}, 2+$self->fheight);
     ();
 }
 
