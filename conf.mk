e6e04a28702a58ce04e8c6b6a371b47b956736a3
Paul Seyfert <paul.seyfert@mib.infn.it>
2016-12-25 02:02:51 +0100

    avoid concatenating conf files
    implement sourcing configuration in shell loop in shellexrc

--- conf.mk
+++ conf.mk
@@ -1,8 +1,11 @@
-INSTALL_TARGETS += install-conf
+INSTALL_TARGETS += install-conf install-rc
+CLEAN_TARGETS += clean-shellexrc
+ALL_TARGETS += conf/shellexrc
 
 default_confs := 10-autoexec 20-nobeep 40-escape 40-home_end 40-setprompt 40-sigint 90-hist 99-clear
 
 install-conf:
+	echo "[INSTALL] $@"
 	$(INSTALL) -d -m 0755 $(DESTDIR)$(PREFIX)$(LIBDIR)/shellex/conf
 	for file in $(wildcard conf/*); \
 	do \
@@ -13,3 +16,15 @@ install-conf:
 	do \
 		[ -e $(DESTDIR)$(SYSCONFDIR)/shellex/$${link} ] || ln -s $(PREFIX)$(LIBDIR)/shellex/conf/$${link} $(DESTDIR)$(SYSCONFDIR)/shellex; \
 	done
+
+conf/shellexrc: conf/shellexrc.in
+	echo "[SED] $@"
+	$(SED) $(sed_replace_vars) $< > $@
+
+install-rc: conf/shellexrc
+	echo "[INSTALL] $@"
+	$(INSTALL) -m 0644 conf/shellexrc $(DESTDIR)$(SYSCONFDIR)/shellexrc
+
+clean-shellexrc:
+	echo "[CLEAN] conf/shellexrc"
+	rm -f conf/shellexrc
